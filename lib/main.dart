9d05774fddd5da4da4a0d360b35d170d01efa1ce
VIKRAMADITYA <vikramadityapratap35@gmail.com>
2023-12-09 14:20:26 +0530

    new code written🎉

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/services.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -7,119 +11,269 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String email = '';
+  String message = '';
+  String token = '';
+  String toEmail = '';
 
-  void _incrementCounter() {
+
+  Future<void> fetchData() async {
+    const url = "https://free-tempmail-api.p.rapidapi.com/newmail";
+
+    final headers = {
+      "X-RapidAPI-Key": "YOUR_API_KEY",
+      "X-RapidAPI-Host": "free-tempmail-api.p.rapidapi.com"
+    };
+
+    final response = await http.get(Uri.parse(url), headers: headers);
+    final info = jsonDecode(response.body);
+    final email = info["newmail"]["email"].toString();
+    final token = info["newmail"]["token"].toString();
+
+    const mailsUrl = "https://free-tempmail-api.p.rapidapi.com/mails";
+    final mailsHeaders = {
+      "mailtoken": token,
+      "X-RapidAPI-Key": "YOUR_API_KEY",
+      "X-RapidAPI-Host": "free-tempmail-api.p.rapidapi.com"
+    };
+
+    final mailsResponse = await http.get(Uri.parse(mailsUrl), headers: mailsHeaders);
+    final mailsInfo = jsonDecode(mailsResponse.body);
+    final message = mailsInfo["message"].toString();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      this.email = email;
+      this.token = token;
+      this.message = message;
+    });
+  }
+
+  Future<void> updateMessage() async {
+    const mailsUrl = "https://free-tempmail-api.p.rapidapi.com/mails";
+    final mailsHeaders = {
+      "mailtoken": token,
+      "X-RapidAPI-Key": "YOUR_API_KEY",
+      "X-RapidAPI-Host": "free-tempmail-api.p.rapidapi.com"
+    };
+
+    final mailsResponse = await http.get(Uri.parse(mailsUrl), headers: mailsHeaders);
+    final mailsInfo = jsonDecode(mailsResponse.body);
+    final updatedMessage = mailsInfo["mails"][0]["intro"].toString();
+    final toEmail = mailsInfo["mails"][0]["from"]["address"].toString();
+    debugPrint(mailsInfo);
+    setState(() {
+      message = updatedMessage;
+      this.toEmail = toEmail;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('FukEmail',
+          style: TextStyle(
+          color: Colors.black,
+          fontWeight: FontWeight.w500,
+        ),),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal:0.0,vertical:10),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 200,
+                    child: Image.asset("assets/mail.png"),
+                  ),
+                  const Center(
+                    child: Text("The temporary mail service allows you to obtain temporary or disposable emails for use as you wish.",
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.purple),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: fetchData,
+                  child: const Text('Generate Email'),
+                ),
+                const SizedBox(width: 20),
+                ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.green),
+                    foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  ),
+                  onPressed: updateMessage,
+                  child: const Text('Refresh Message'),
+                ),
+              ],
+            ),
+
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.95,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Email: $email',
+                        style: const TextStyle(fontSize: 13.5),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text: email));
+                          final snackBar = SnackBar(
+                            content: const Text('Copied'),
+                            behavior: SnackBarBehavior.floating,
+                            shape : RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              side: const BorderSide(color: Colors.purple, width: 2),
+                            ),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        },
+                        icon: const Icon(Icons.copy_all),
+                        color: Colors.purple,
+
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.95,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 3,
+                  ),
+                ],
+              ),
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 10),
+                  Text(
+                    'To : $toEmail',
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    'Message: $message',
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text: message));
+                          final snackBar = SnackBar(
+                            content: const Text('Copied'),
+                            behavior: SnackBarBehavior.floating,
+                            shape : RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              side: const BorderSide(color: Colors.purple, width: 2),
+                            ),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                        },
+                        icon: const Icon(Icons.copy_all),
+                        color: Colors.purple,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            const Center(
+              child: Text("version 1.0.0",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w200,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: const Padding(
+        padding: EdgeInsets.all(5.0),
+        child: Text('Developed By Vikramaditya', textAlign: TextAlign.center,),
+      ),
     );
   }
 }
